589efa97ffdf1ac0edcf6b96bd51e18a2850bd09
kjuiop <copying_y@naver.com>
2026-05-20 18:27:26 +0900

    [#11] fix: checkstyle 빌드 오류 해결

--- ad-admin-api/src/main/java/org/giglab/ad/admin/api/AdAdminApiApplication.java
+++ ad-admin-api/src/main/java/org/giglab/ad/admin/api/AdAdminApiApplication.java
@@ -3,9 +3,11 @@ package org.giglab.ad.admin.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** 광고 관리자 API 애플리케이션. */
 @SpringBootApplication
 public class AdAdminApiApplication {
 
+  /** 애플리케이션 진입점. */
   public static void main(String[] args) {
     SpringApplication.run(AdAdminApiApplication.class, args);
   }
